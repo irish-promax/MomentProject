e77d235d6a5f5ec3c0c1048bd9008b3c9a78760f
Iman Irish Daniel - Dev <imanirishdaniel-dev@iMac.local>
2023-01-06 21:56:47 +0800

    Update - Interface

--- src/screens/app/SLogout/index.js
+++ src/screens/app/SLogout/index.js
@@ -1,28 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-import {Text, TextInput, View} from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Button from '../../../components/Buttons';
+import { colors } from '../../../utils/colors';
 import { styles } from './styles';
 
-const SLogout = ({navigation}) => {
+const SLogout = ({ navigation }) => {
     const onBack = () => {
         navigation.navigate('SignIn')
     }
-    return(
+    return (
         <SafeAreaView style={styles.container}>
             <View >
-                <Text style={styles.title}>Successfuly Log out lol</Text>
-                
+                <Text style={{
+                    fontSize: 30,
+                    fontWeight: '600',
+                    color: colors.white,
+                    textAlign: 'center',
+                    margin: 50
+                }}>Successfuly Log</Text>
+
             </View>
-            
+
             <View >
-                <Button onPress={onBack} title="Back to splash"/>
+                <Button onPress={onBack} title="Back to Log In" />
             </View>
-            
+
 
         </SafeAreaView>
-      
+
     )
 }
 
